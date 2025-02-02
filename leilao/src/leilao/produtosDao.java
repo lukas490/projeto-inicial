393626b928266feea09f1457a42447600aef6156
lukas490 <lucasboff2808@gmail.com>
2025-02-02 19:32:37 -0300

    metodo consultar

--- leilao/src/leilao/produtosDao.java
+++ leilao/src/leilao/produtosDao.java
@@ -6,6 +6,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class produtosDao {
@@ -40,6 +42,27 @@ public class produtosDao {
             System.out.println("Erro ao conectar: " + ex.getMessage());
             return ex.getErrorCode();
         }
+    }
+    
+    public List<produtos> consultar (String nome){
+        List<produtos> lista = new ArrayList<>();
+    try {
+        st = conn.prepareStatement("SELECT * FROM produtos");
+        
+        rs = st.executeQuery();
+        
+        while (rs.next()) {
+            produtos p = new produtos();
+            p.setId(Integer.parseInt(rs.getString("id")));
+            p.setNome(rs.getString("nome"));
+            p.setValor(Double.parseDouble(rs.getString("valor")));
+            p.setStatus(rs.getString("status"));
+            lista.add(p);
+        }
+    } catch (SQLException ex) {
+        System.out.println("Erro ao consultar: " + ex.getMessage());
+    }
+    return lista;
     }
     
     public void desconectar(){
