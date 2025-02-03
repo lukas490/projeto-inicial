07967c5daf8aab8d53d6d4b2874edb772b286a35
lukas490 <lucasboff2808@gmail.com>
2025-02-02 23:13:45 -0300

    implementando botão para a tela de vendas

--- leilao/src/view/listagem.java
+++ leilao/src/view/listagem.java
@@ -63,6 +63,7 @@ public class listagem extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         btnCadastro = new javax.swing.JButton();
         btnVenderProduto = new javax.swing.JButton();
+        btnConsultarVendas = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -96,6 +97,13 @@ public class listagem extends javax.swing.JFrame {
             }
         });
 
+        btnConsultarVendas.setText("CONSULTAR VENDAS");
+        btnConsultarVendas.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnConsultarVendasActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -109,6 +117,8 @@ public class listagem extends javax.swing.JFrame {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(btnCadastro)
+                        .addGap(51, 51, 51)
+                        .addComponent(btnConsultarVendas)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(btnVenderProduto))
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -123,7 +133,8 @@ public class listagem extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 28, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnCadastro)
-                    .addComponent(btnVenderProduto))
+                    .addComponent(btnVenderProduto)
+                    .addComponent(btnConsultarVendas))
                 .addContainerGap())
         );
 
@@ -183,6 +194,12 @@ public class listagem extends javax.swing.JFrame {
 }
     }//GEN-LAST:event_btnVenderProdutoActionPerformed
 
+    private void btnConsultarVendasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnConsultarVendasActionPerformed
+       vendas telaNova = new vendas();
+       telaNova.setVisible(true);
+       this.dispose();
+    }//GEN-LAST:event_btnConsultarVendasActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -220,6 +237,7 @@ public class listagem extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnCadastro;
+    private javax.swing.JButton btnConsultarVendas;
     private javax.swing.JButton btnVenderProduto;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
